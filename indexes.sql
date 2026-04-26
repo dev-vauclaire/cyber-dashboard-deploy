6ff33b822302daefeedda5f3f2b47729d8a800cf
Dev Vauclaire <dev@ch-vauclaire.fr>
2026-04-26 16:20:44 +0200

    chore : moove sql script in postgres directory

--- indexes.sql
+++
@@ -1,3 +0,0 @@
--- Index pour accélérer les recherches du corrélateur
-CREATE INDEX idx_attacks_status ON attacks(correlation_status) WHERE correlation_status = 'pending';
-CREATE INDEX idx_attacks_attacker_ip ON attacks(attacker_ip);
--- insert.sql
+++
@@ -1,5 +0,0 @@
--- Insertion des types de base
-INSERT INTO sensor_types (code, label, category) VALUES 
-('lurio', 'Lurio Honeypot', 'leurre'),
-('detoxio', 'Detoxio Scanner', 'détection'),
-('waf', 'Web Application Firewall', 'protection');
